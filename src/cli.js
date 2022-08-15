c005e14eac2440835eaba8b51cf28adb41b801a4
DungTD10 <mducdung255@gmail.com>
2022-08-15 14:02:34 +0700

    feat: release version 4.0.0

--- src/cli.js
+++ src/cli.js
@@ -8,10 +8,9 @@ function parserArgumentsIntoOptions(rawArgs) {
          '--help': Boolean,
          '--all': Boolean,
          '--git': Boolean,
-         '--install': Boolean,
          '--nodemon': Boolean,
          '--eslint': Boolean,
-         '--pre-commit-hook': Boolean,
+         '--commitlint': Boolean,
          '--editorconfig': Boolean,
          '--ts': Boolean,
          '--name': String,
@@ -22,10 +21,9 @@ function parserArgumentsIntoOptions(rawArgs) {
          '-a': '--all',
          '-h': '--help',
          '-g': '--git',
-         '-i': '--install',
          '-r': '--nodemon',
          '-l': '--eslint',
-         '-p': '--pre-commit-hook',
+         '-c': '--commitlint',
          '-e': '--editorconfig',
          '-n': '--name',
       },
@@ -36,21 +34,19 @@ function parserArgumentsIntoOptions(rawArgs) {
    );
 
    let packageManager = 'pnpm';
+   const packageManagerArg = args['--yarn'] || args['--npm'] || args['--pnpm'];
 
-   if (args['--yarn']) {
-      packageManager = 'yarn';
-   } else if (args['--npm']) {
-      packageManager = 'npm';
+   if (packageManagerArg) {
+      packageManager = packageManagerArg.slice(2);
    }
 
    return {
       help: args['--help'] || false,
       skipPrompts: args['--all'] || false,
       git: args['--git'] || false,
-      runInstall: args['--install'] || false,
       nodemon: args['--nodemon'] || false,
       eslint: args['--eslint'] || false,
-      preCommitHook: args['--pre-commit-hook'] || false,
+      commitLint: args['--commitlint'] || false,
       editorConfig: args['--editorconfig'] || false,
       ts: args['--ts'] || false,
       packageManager,
@@ -59,59 +55,48 @@ function parserArgumentsIntoOptions(rawArgs) {
    };
 }
 
-// eslint-disable-next-line
 async function promptForMissingOptions(options) {
    const questions = [];
 
-   questions.push({
-      type: 'input',
-      name: 'name',
-      message: 'Choose your project name:',
-      default: 'my-app',
-   });
-
-   questions.push({
-      type: 'list',
-      name: 'packageManager',
-      message: 'Choose your favorite package manager:',
-      choices: ['npm', 'yarn', 'pnpm'],
-      default: 'pnpm',
-   });
-
-   questions.push({
-      type: 'confirm',
-      name: 'ts',
-      message: 'Integrate typescript to project (your .ts file will be in src/ folder) ?',
-      default: false,
-   });
-
-   questions.push({
-      type: 'confirm',
-      name: 'git',
-      message: 'Create a new git repository ?',
-      default: false,
-   });
-
-   questions.push({
-      type: 'confirm',
-      name: 'runInstall',
-      message: 'Install node_modules',
-      default: true,
-   });
-
-   questions.push({
-      type: 'confirm',
-      name: 'eslint',
-      message: 'Integrate eslint(coding style guide) ?',
-      default: false,
-   });
-
-   questions.push({
-      type: 'confirm',
-      name: 'editorConfig',
-      message: 'Create .editorconfig file ?',
-      default: false,
-   });
+   questions.push(
+      {
+         type: 'input',
+         name: 'name',
+         message: 'Choose your project name:',
+         default: 'my-app',
+      },
+      {
+         type: 'list',
+         name: 'packageManager',
+         message: 'Choose your favorite package manager:',
+         choices: ['npm', 'yarn', 'pnpm'],
+         default: 'pnpm',
+      },
+      {
+         type: 'confirm',
+         name: 'ts',
+         message: 'Integrate typescript to project (your .ts file will be in src/ folder) ?',
+         default: false,
+      },
+      {
+         type: 'confirm',
+         name: 'git',
+         message: 'Create a new git repository ?',
+         default: false,
+      },
+      {
+         type: 'confirm',
+         name: 'eslint',
+         message: 'Integrate eslint(coding style guide) ?',
+         default: false,
+      },
+      {
+         type: 'confirm',
+         name: 'editorConfig',
+         message: 'Create .editorconfig file ?',
+         default: false,
+      },
+   );
 
    if (options.ts) {
       questions.push({
@@ -125,8 +110,8 @@ async function promptForMissingOptions(options) {
    if (options.eslint && options.git) {
       questions.push({
          type: 'confirm',
-         name: 'preCommitHook',
-         message: 'Use pre-commit hook(check eslint before commit) ?',
+         name: 'commitLint',
+         message: 'Integrate commitlint (git commit style guide) ?',
          default: false,
       });
    }
@@ -137,10 +122,9 @@ async function promptForMissingOptions(options) {
       ...options,
       packageManager: answers.packageManager || options.packageManager,
       git: answers.git || options.git,
-      runInstall: answers.runInstall || options.runInstall,
       nodemon: answers.nodemon || options.nodemon,
       eslint: answers.eslint || options.eslint,
-      preCommitHook: answers.preCommitHook || options.preCommitHook,
+      commitLint: answers.commitLint || options.commitLint,
       editorConfig: answers.editorConfig || options.editorConfig,
       name: answers.name || options.name,
       ts: answers.ts || options.ts,
@@ -150,22 +134,23 @@ async function promptForMissingOptions(options) {
 function showHelp() {
    const helps = [
       'USAGE: ts-gun [OPTION]',
-      'Initalize typescript project with some essential tools',
-      'Example: ts-gun -s\n',
+      'Initalize typescript project with some essential tools(nodemon, eslint, husky, commitlint, editorconfig)',
+      'Example: ts-gun                       use the cli. Recommended',
+      '         ts-gun -an <project-name>    install all tools in <project-name> with pnpm as package manager (this will skips the cli)',
       'OPTIONS:',
-      '          --npm                  use npm as package manager',
-      '          --pnpm                 use pnpm as package manager',
-      '          --yarn                 use yarn as package manager',
-      '   -h,    --help                 show help menu',
-      '   -a,    --all                  use pnpm as package manager and install all tools(eslint, install node_module, nodemon, editorconfig, pre-commit-hook, init git repository)',
-      '   -n,    --name                 will create a folder with given name in current directory. other configuration files will be placed inside this folder',
-      '   -t,    --ts                   initialize typescript to project',
-      '   -g,    --git                  initialize new git repository',
-      '   -i,    --install              install node_modules and dependencies for eslint(if eslint option is true), typescript(if ts option is true)',
-      '   -r,    --nodemon              integrate nodemon to project',
-      '   -e,    --eslint               integrate eslint to project. Use airbnb-base style guide',
-      '   -p,    --pre-commit-hook      use pre-commit-hook. Will run script "lint" and "test" in package.json file. .git folder is required',
-      '   -e,    --editorconfig         create .editorconfig file\n',
+      '        --npm                  npm as package manager',
+      '        --pnpm                 pnpm as package manager',
+      '        --yarn                 yarn as package manager',
+      '        --ts                   integrate typescript to project',
+      '   -h,  --help                 show help menu',
+      '   -a,  --all                  use pnpm as package manager and install all tools(eslint, install node_module, nodemon, editorconfig, commitlint, init git repository)',
+      '   -n,  --name                 will create a folder with given name in current directory. other configuration files will be placed inside this folder',
+      '   -t,  --ts                   initialize typescript to project',
+      '   -g,  --git                  initialize new git repository',
+      '   -r,  --nodemon              integrate nodemon to project',
+      '   -l,  --eslint               integrate eslint to project. Use airbnb-base style guide',
+      '   -c,  --commitlint           use pre-commit-hook using husky. Will run script "test" and "lint" in package.json file. Besides that, check commit message for each commit based on commitlint',
+      '   -e,  --editorconfig         create .editorconfig file\n',
       'NOTE: this package is not stable yet, if you have any problem, please go to: https://www.npmjs.com/package/ts-gun for more details\n',
    ];
 
@@ -193,7 +178,7 @@ export async function cli(args) {
          runInstall: true,
          nodemon: true,
          eslint: true,
-         preCommitHook: true,
+         commitLint: true,
          editorConfig: true,
          ts: true,
       };
--- src/main.js
+++ src/main.js
@@ -9,11 +9,6 @@ import chalk from 'chalk';
 const access = promisify(fs.access);
 const copy = promisify(ncp);
 
-function showMissingInstallErr(packages) {
-   const errMsg = `\n%s option --install and -i are disabled.Retry the comand with --install or -i option and a package manager.Or you can install following packages manually:\n${packages.join(' ')}\n`;
-   return console.warn(errMsg, chalk.yellow.bold('WARNING:'));
-}
-
 async function run(cmds, options) {
    try {
       const result = await execa(...cmds, {
@@ -21,7 +16,7 @@ async function run(cmds, options) {
       });
 
       if (result.failed) {
-         return Promise.reject(new Error(`Failed to run ${cmds[0]} ${cmds[1].join(' ')}`));
+         return Promise.reject(Error(`Failed to run ${cmds[0]} ${cmds[1].join(' ')}`));
       }
 
       return null;
@@ -32,25 +27,34 @@ async function run(cmds, options) {
 
 async function initGit(options) {
    await run(['git', ['init']], options);
-   await copy(options.templateDirectory.concat('/gitignore'), options.targetDirectory.concat('/.gitignore'), { clobber: false });
+
+   await copy(
+      options.templateDirectory.concat('/gitignore'),
+      options.targetDirectory.concat('/.gitignore'),
+      { clobber: false },
+   );
 }
 
 async function initTs(options) {
-   await copy(options.templateDirectory.concat('/typescripts'), options.targetDirectory, { clobber: false });
+   await copy(
+      options.templateDirectory.concat('/typescripts'),
+      options.targetDirectory,
+      { clobber: false },
+   );
 
    const installPackages = ['@types/node', 'typescript', 'ts-node'];
 
-   if (options.runInstall) {
-      const installAlias = options.packageManager === 'yarn' ? 'add' : 'install';
-      const installCommand = [options.packageManager, [installAlias, '-D', ...installPackages]];
-      return run(installCommand, options);
-   }
-
-   return showMissingInstallErr(installPackages);
+   const installAlias = options.packageManager === 'yarn' ? 'add' : 'install';
+   const installCommand = [options.packageManager, [installAlias, '-D', ...installPackages]];
+   return run(installCommand, options);
 }
 
 async function eslintInit(options) {
-   await copy(options.templateDirectory.concat('/eslint'), options.targetDirectory, { clobber: false });
+   await copy(
+      options.templateDirectory.concat('/eslint'),
+      options.targetDirectory,
+      { clobber: false },
+   );
 
    const installPackages = [
       'eslint',
@@ -60,45 +64,62 @@ async function eslintInit(options) {
       '@typescript-eslint/parser',
    ];
 
-   if (options.runInstall) {
-      const installAlias = options.packageManager === 'yarn' ? 'add' : 'install';
+   const installAlias = options.packageManager === 'yarn' ? 'add' : 'install';
 
-      const installlArgs = [
-         installAlias,
-         '-D',
-         ...installPackages,
-      ];
+   const installlArgs = [
+      installAlias,
+      '-D',
+      ...installPackages,
+   ];
 
-      return run([options.packageManager, installlArgs], options);
-   }
-
-   return showMissingInstallErr(installPackages);
+   return run([options.packageManager, installlArgs], options);
 }
 
 async function nodemonInit(options) {
-   await copy(options.templateDirectory.concat('/nodemon'), options.targetDirectory, { clobber: false });
-
-   if (options.runInstall) {
-      const installAlias = options.packageManager === 'yarn' ? 'add' : 'install';
-
-      const installlArgs = [
-         installAlias,
-         '-D',
-         'nodemon',
-      ];
-
-      return run([options.packageManager, installlArgs], options);
+   if (!options.ts) {
+      throw Error('nodemon is current not available for JS projects');
    }
 
-   return showMissingInstallErr(['nodemon']);
+   await copy(
+      options.templateDirectory.concat('/nodemon'),
+      options.targetDirectory,
+      { clobber: false },
+   );
+
+   const installAlias = options.packageManager === 'yarn' ? 'add' : 'install';
+
+   const installlArgs = [
+      installAlias,
+      '-D',
+      'nodemon',
+   ];
+
+   return run([options.packageManager, installlArgs], options);
 }
 
-async function preCommitHookInit(options) {
-   await copy(options.templateDirectory.concat('/preCommitHook'), options.targetDirectory.concat('/.git/hooks'), { clobber: false });
+async function integrateCommitLint(options) {
+   if (!options.eslint || !options.git) {
+      throw Error('commitlint can not work without git and eslint');
+   }
+
+   const installAlias = options.packageManager === 'yarn' ? 'add' : 'install';
+   const installPackages = ['@commitlint/cli', '@commitlint/config-conventional', 'husky'];
+   await run([options.packageManager, [installAlias, '-D', ...installPackages]], options);
+   await run(['npx', ['husky', 'install']], options);
+
+   await copy(
+      options.templateDirectory.concat('/git-hooks'),
+      options.targetDirectory.concat('/.husky'),
+      { clobber: false },
+   );
 }
 
 async function editorconfigInit(options) {
-   await copy(options.templateDirectory.concat('/editorconfig'), options.targetDirectory, { clobber: false });
+   await copy(
+      options.templateDirectory.concat('/editorconfig'),
+      options.targetDirectory,
+      { clobber: false },
+   );
 }
 
 export async function createTsProject(opts) {
@@ -107,66 +128,79 @@ export async function createTsProject(opts) {
       targetDirectory: opts.targetDirectory || process.cwd(),
    };
 
-   const currentFileUrl = import.meta.url;
-
-   const templateDir = path.resolve(
-      new URL(currentFileUrl).pathname,
-      '../../templates',
-   );
-
-   options.templateDirectory = templateDir;
-
    try {
-      await access(templateDir, fs.constants.R_OK);
-   } catch (err) {
-      console.error(`%s ${err.toString()}`, chalk.red.bold('ERROR:'));
-      process.exit(1);
+      const currentFileUrl = import.meta.url;
+
+      const templateDir = path.resolve(
+         new URL(currentFileUrl).pathname,
+         '../../templates',
+      );
+
+      options.templateDirectory = templateDir;
+
+      try {
+         await access(templateDir, fs.constants.R_OK);
+      } catch (err) {
+         console.error(`%s ${err.toString()}`, chalk.red.bold('ERROR:'));
+         process.exit(1);
+      }
+
+      console.info('%s Prepare to initalize project', chalk.bold.green('>>'));
+
+      const tasks = new Listr([
+         {
+            title: `Create folder ${options.name}`,
+            task: () => fs.mkdirSync(options.name),
+            enabled: () => options.name,
+         },
+         {
+            title: 'Initialize new git repository',
+            task: () => initGit(options),
+            enabled: () => options.git,
+         },
+         {
+            title: 'Initialize typescript',
+            task: () => initTs(options),
+            enabled: () => options.ts,
+         },
+         {
+            title: 'Integrate eslint',
+            task: () => eslintInit(options),
+            enabled: () => options.eslint,
+         },
+         {
+            title: 'Create .editorconfig file',
+            task: () => editorconfigInit(options),
+            enabled: () => options.editorConfig,
+         },
+         {
+            title: 'Integrate nodemon',
+            task: () => nodemonInit(options),
+            enabled: () => options.nodemon,
+         },
+         {
+            title: 'Integrate git husky + commitlint',
+            task: () => integrateCommitLint(options),
+            enabled: () => options.commitLint,
+         },
+      ]);
+
+      const startTime = Date.now();
+      await tasks.run();
+      const durationSec = Math.ceil((Date.now() - startTime) / 1000);
+      const projectName = options.name ? options.name.concat(' ') : '';
+
+      console.info(
+         '%s Project %sis ready to go now. Happy coding! (%ss)',
+         chalk.bold.green('DONE.'),
+         chalk.blue(projectName),
+         chalk.blue(durationSec),
+      );
+   } catch (error) {
+      await run(['rm', ['-rf', options.targetDirectory]], options);
+
+      console.info(
+         chalk.bold.red('ERROR: Can not create project. Please check the causes above.'),
+      );
    }
-
-   console.info('%s Prepare to initalize project', chalk.bold.green('>>'));
-
-   const tasks = new Listr([
-      {
-         title: `Create folder ${options.name}`,
-         task: () => fs.mkdirSync(options.name),
-         enabled: () => options.name,
-      },
-      {
-         title: 'Initialize new git repository',
-         task: () => initGit(options),
-         enabled: () => options.git,
-      },
-      {
-         title: 'Initialize typescript',
-         task: () => initTs(options),
-         enabled: () => options.ts,
-      },
-      {
-         title: 'Integrate eslint',
-         task: () => eslintInit(options),
-         enabled: () => options.eslint,
-      },
-      {
-         title: 'Create .editorconfig file',
-         task: () => editorconfigInit(options),
-         enabled: () => options.editorConfig,
-      },
-      {
-         title: 'Integrate nodemon',
-         task: () => nodemonInit(options),
-         enabled: () => options.nodemon,
-      },
-      {
-         title: 'Integrate git pre-commit hook',
-         task: () => preCommitHookInit(options),
-         enabled: () => options.preCommitHook,
-      },
-   ]);
-
-   const startTime = Date.now();
-   await tasks.run();
-   const durationSec = Math.ceil((Date.now() - startTime) / 1000);
-   const projectName = options.name ? options.name.concat(' ') : '';
-   console.info('%s Project %sis ready to go now. Happy coding! (%ss)', chalk.bold.green('DONE.'), chalk.blue(projectName), chalk.blue(durationSec));
-   return true;
 }
